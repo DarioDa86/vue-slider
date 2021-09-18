da6f45d0c4beb5f93ca763b0f3e36d5a9b14d2dd
DarioDa86 <dario.dagnelli@gmail.com>
2021-09-18 10:28:02 +0200

    Inserite img e fixati errori. Es terminato

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ const app = new Vue({
     el: "#root",
     data: {
         images: [
-            "https://www.bluedolphinroma.it/wp-content/uploads/2020/06/barca-elettrica.jpg",
-            "https://www.gelestatic.it/thimg/TnIKzLcaxbbtDV5v6uVzpHEi82Q=/fit-in/960x540/https%3A//www.lastampa.it/image/contentid/policy%3A1.38765328%3A1587916803/cache_12009771.jpg%3Ff%3Ddetail_558%26h%3D720%26w%3D1280%26%24p%24f%24h%24w%3Dcb41667"
+            "./img/barca-1.jpeg",
+            "./img/barca-2.jpg",
+            "./img/barca-3.jpg",
+            "./img/barca-4.jpg",
+            "./img/barca-5.jpg"
         ],
         imageIndex: 0
     },
@@ -11,20 +14,20 @@ const app = new Vue({
         nextImage: function () {
             this.imageIndex++;
 
-            if ( this.imageIndex >= this.image.length ) {
+            if ( this.imageIndex >= this.images.length ) {
                 this.imageIndex = 0;
             }
         },
         prevImage: function () {
             this.imageIndex--;
-
+ 
             if ( this.imageIndex == -1 ) {
-                this.imageIndex = this.image.length -1;
+                this.imageIndex = this.images.length -1;
             }
         },
         isBulletActive: function (indexBullet) {
             if ( this.imageIndex == indexBullet ) {
-                return 'active' ;
+                return 'active';
             } 
             return '';
         },
